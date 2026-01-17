7c65097ffbd2c0cb24f16de07cc6cf593429e249
Treats <treats@MrToot@foolstacked.com>
2026-01-17 15:01:59 +0800

    feat: add asn1 encoding for some types

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"crypto/rand"
+	"encoding/binary"
 	"flag"
 	"fmt"
+	"math"
 	"net/http"
 	"os"
 	"sync"
@@ -126,7 +130,7 @@ func handleGetRotation(w http.ResponseWriter, r *http.Request) {
 func main() {
 	argsNum := len(os.Args)
 
-	mode := flag.String("mode", "encrypt", "The mode to run the program in")
+	mode := flag.String("mode", "default", "The mode to run the program in")
 	flag.Parse()
 
 	switch *mode {
@@ -142,6 +146,8 @@ func main() {
 		interfaces.Exec()
 	case "context":
 		contxt.Exec()
+	default:
+		ASN1Encoding()
 	}
 
 }
@@ -151,4 +157,138 @@ func cryptoFun(filename string) {
 	file := iofile.ReadFile(filename)
 
 	crypt.FreqAnalysis(string(file))
+}
+
+type ASN1_Types int
+
+const (
+	BOOLEAN ASN1_Types = iota
+	INTEGER
+	BIT_STR
+	OCTET_STR
+	NULL
+	OBJ_IDENT
+	OBJ_DESC
+	EXTERNAL
+	REAL
+	ENUM
+	EMBED_PDV
+	UTF8_STR
+	CHOICE
+	RELATIVE_OID
+	TIME
+	SEQ
+	SEQ_OF
+	SET
+	SET_OF
+)
+
+func ASN1Encoding() {
+
+	var buf bytes.Buffer
+
+	const size = 256
+
+	egData := make([]byte, size)
+	_, err := rand.Read(egData)
+
+	if err != nil {
+		fmt.Println("Unable to generate random data")
+	}
+
+	curSize := encodeASN1(egData, OCTET_STR, &buf)
+
+	fmt.Printf("Current size: %d\n", curSize)
+	fmt.Printf("%#v\n", buf.Bytes())
+}
+
+/**
+* Encode an ASN1 type
+ */
+func encodeASN1(data []byte, asnType ASN1_Types, buf *bytes.Buffer) int {
+	size := 0
+
+	dataLen := len(data)
+
+	switch asnType {
+	case INTEGER:
+		fmt.Println("Encoding INT")
+
+		_, err := buf.Write([]byte{0x02, byte(dataLen)})
+
+		if err != nil {
+			fmt.Println("Failed to write to buffer")
+		}
+		_, err = buf.Write(data)
+
+		if err != nil {
+			fmt.Println("Failed to write to buffer")
+		}
+
+		size += 2 + dataLen // INT Tag, Length, Data
+
+	case OCTET_STR:
+		if dataLen <= 0x7F {
+			fmt.Println("Encoding OCTET STR")
+			buf.Write([]byte{0x04, byte(dataLen)})
+			buf.Write(data)
+
+			size += 2 + dataLen
+
+		} else {
+
+			fmt.Println("Encoding extended length OCTET STR")
+
+			numOfBytes := getNumOfBytesFromInt(dataLen)
+
+			buf.Write([]byte{0x04, 0x80 | byte(numOfBytes)})
+			writeAsByteSlice(buf, dataLen)
+			buf.Write(data)
+
+			size += 2 + numOfBytes + dataLen // Tag, Size of Len, Len, Len of Data
+		}
+	case SEQ:
+		fmt.Println("Encoding a SEQUENCE")
+		buf.Write([]byte{0x30, byte(dataLen)})
+		buf.Write(data)
+
+		size += 2 + dataLen
+	case SET:
+		fmt.Println("Encoding a SET")
+		buf.Write([]byte{0x31, byte(dataLen)})
+		buf.Write(data)
+
+		size += 2 + dataLen
+	default:
+		fmt.Println("No such ASN type handled")
+	}
+
+	return size
+}
+
+func writeAsByteSlice(buf *bytes.Buffer, i int) {
+
+	switch {
+	case i >= math.MinInt8 && i <= math.MaxInt8:
+		binary.Write(buf, binary.BigEndian, int8(i))
+	case i >= math.MinInt16 && i <= math.MaxInt16:
+		binary.Write(buf, binary.BigEndian, int16(i))
+	case i >= math.MinInt32 && i <= math.MaxInt32:
+		binary.Write(buf, binary.BigEndian, int32(i))
+	case i >= math.MinInt64 && i <= math.MaxInt64:
+		binary.Write(buf, binary.BigEndian, int64(i))
+	default:
+		fmt.Println("Integer is out of range")
+	}
+}
+
+func getNumOfBytesFromInt(i int) int {
+	counter := 0
+
+	for i > 0 {
+		i >>= 8
+		counter += 1
+	}
+
+	return counter
 }
